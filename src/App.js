f5a38c899e75c6bffa72353af40b4d37d9f77c7d
marlom46 <80200977+marlom46@users.noreply.github.com>
2021-07-01 20:15:52 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import web3 from './web3';
+import lottery from './lottery';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    
+    manager: ''
+  };
+  
+  componentDidMount = async () => {
+    
+    const manager = await lottery.methods.manager().call();
+    this.setState({ manager });
+  }
+
+  render = () => {
+    return (
+      <div className="App" >
+        <h3>Lottery Contract</h3>
+        <p>Managed by: {this.state.manager}</p>
+      </div>
+    );
+  }  
 }
 
 export default App;
